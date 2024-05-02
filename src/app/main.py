73ceb317441635edd43573d323569bea14b27384
Reinaldo Agostinho de Souza Filho <reinaldo.filho@bigdatacorp.com.br>
2024-05-02 10:24:13 -0300

    [Try-Fix] Requirements

--- src/app/main.py
+++ src/app/main.py
@@ -2,15 +2,16 @@ from flask import Flask, request, jsonify
 from flask_basicauth import BasicAuth
 from textblob import TextBlob
 from sklearn.linear_model import LinearRegression
+from google.cloud import translate_v2 as translate
 import pickle
 import os
 
 colunas = ['tamanho','ano','garagem']
-modelo = pickle.load(open('../../models/modelo.sav','rb'))
+modelo = pickle.load(open('models/modelo.sav','rb'))
 
 app = Flask(__name__)
-app.config['BASIC_AUTH_USERNAME'] = os.environ.get('BASIC_AUTH_USERNAME')
-app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
+app.config['BASIC_AUTH_USERNAME'] = "reinaldo"#os.environ.get('BASIC_AUTH_USERNAME')
+app.config['BASIC_AUTH_PASSWORD'] = "alura"#os.environ.get('BASIC_AUTH_PASSWORD')
 
 basic_auth = BasicAuth(app)
 
@@ -22,8 +23,8 @@ def home():
 @basic_auth.required
 def sentimento(frase):
     tb = TextBlob(frase)
-    tb_en = tb.translate(to='en')
-    polaridade = tb_en.sentiment.polarity
+    
+    polaridade = tb.sentiment.polarity
     return "polaridade: {}".format(polaridade)
 
 @app.route('/cotacao/', methods=['POST'])
